48295882f56270938aa7af3c2277f9fd4640bb16
DESKTOP-I1FA69H\jp <nevermores88@hotmail.com>
2020-09-28 16:40:34 +0900

    no message

---
+++ 05 Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2.0f;
+    [SerializeField] float yPush = 15.0f;
+    [SerializeField] AudioClip[] ballSounds;
+    [SerializeField] float randomFactor = 0.2f;
+
+    Vector2 paddleToBallVector;
+    bool hasStarted = false;
+
+    AudioSource myAudioSource;
+    Rigidbody2D myRigidBody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+        myAudioSource = GetComponent<AudioSource>();
+        myRigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            myRigidBody2D.velocity = new Vector2(xPush, yPush);
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = paddle1.transform.position;
+        transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector2 velocityTweak = new Vector2(UnityEngine.Random.Range(0.0f, randomFactor), UnityEngine.Random.Range(0.0f, randomFactor));
+        if (hasStarted)
+        {
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            myAudioSource.PlayOneShot(clip);
+            myRigidBody2D.velocity += velocityTweak;
+        }
+    }
+}
---
+++ 05 Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float minX = 1.0f;
+    [SerializeField] float maxX = 15.0f;
+    [SerializeField] float screenWidthInUnits = 16.0f;
+
+    GameSession theGameSession;
+    Ball theBall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theGameSession = FindObjectOfType<GameSession>();
+        theBall = FindObjectOfType<Ball>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(GetXPos(), minX, maxX);
+        transform.position = paddlePos;
+    }
+
+    private float GetXPos()
+    {
+        if(theGameSession.IsAutoPlayEnabled())
+        {
+            return theBall.transform.position.x;
+        }
+        else
+        {
+            return Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        }
+    }
+}
