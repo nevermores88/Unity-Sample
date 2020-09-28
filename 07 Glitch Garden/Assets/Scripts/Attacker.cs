d86ea4b444aa21ecb26e873ebe24380b3d54f4b7
DESKTOP-I1FA69H\jp <nevermores88@hotmail.com>
2020-09-28 16:41:40 +0900

    no message

---
+++ 07 Glitch Garden/Assets/Scripts/Attacker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+	[Range (0.0f, 5.0f)] float currentSpeed = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.Translate(Vector2.left * Time.deltaTime * currentSpeed);
+    }
+
+	public void SetMovementSpeed(float speed)
+	{
+		currentSpeed = speed;
+	}
+}
